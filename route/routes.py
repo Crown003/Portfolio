9d1cf536223197f9b5b6004e0c17a5be5a1ea2fe
HARSHIT <harshittiwari4u@gmail.com>
2023-08-18 22:16:25 +0530

    added jinja for loop to render cards in myworksection

--- route/routes.py
+++ route/routes.py
@@ -3,7 +3,48 @@ myWeb  = Blueprint("myWebsite",__name__)
 
 @myWeb.route("/")
 def home():
-	return render_template("index.html")
+	data = {
+		"WebDevlopment":
+			{
+				"firstWeb":
+				{
+					"Title" : "My First Website",
+					"mainImg": './images/firstWeb.jpg',
+					"techUsedImg":[],
+					"link":[]
+				},
+				"thisPortfolio":
+				{
+					"Title" :"This Portfoli Website",
+					"mainImg":'./images/newProtfolio.jpg',
+					"techUsedImg":[],
+					"link":[],
+				}		
+			},
+		"SoftwareDevlopment":
+			{
+				"Test data":
+				{
+					"Title" : "My First Website",
+					"mainImg": './images/firstWeb.jpg',
+					"techUsedImg":[],
+					"link":[]
+				}
+				
+			},
+		"DataScience & AI":
+			{
+				"Test data":
+				{
+					"Title" : "My First Website",
+					"mainImg": './images/firstWeb.jpg',
+					"techUsedImg":[],
+					"link":[]
+				}
+				
+			},
+	}
+	return render_template("index.html",data=data)
 	
 @myWeb.route("/contact")
 def contact():
